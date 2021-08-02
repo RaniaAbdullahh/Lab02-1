fe1bb06426b6c632ca56fdfac82ecc3847da106b
Mohammad-01 <moalhasan432@gmail.com>
2021-08-02 15:33:07 +0300

    add files

---
+++ app.js
@@ -0,0 +1,59 @@
+let name = prompt("What is your name?");
+alert ("Hello "+ name);
+console.log(name);
+
+let q1 = prompt("Do you think my fav food is mansaf ? Answer by Yes / No / Y / N");
+q1 = q1.toLowerCase();
+console.log(q1);
+if (q1 === 'yes' || q1 === 'y' ){
+alert("True, My fav food is mansaf");
+}
+if (q1 === 'no' || q1 === 'n' ){
+alert("False, My fav food is mansaf");
+}
+
+
+
+let q2 = prompt("Do you think my fav sport is basketball ? Answer by Yes / No / Y / N");
+q2 = q2.toLowerCase();
+console.log(q2);
+if (q2 === 'yes' || q2 === 'y' ){
+    alert("True, my fav sport is basketball ");
+    }
+    if (q2 === 'no' || q2 === 'n' ){
+    alert("False, my fav sport is basketball ");
+    }
+    
+
+let q3 = prompt("Do you think I play biano ? Answer by Yes / No / Y / N");
+q3 = q3.toLowerCase();
+console.log(q3);
+if (q3 === 'yes' || q3 === 'y' ){
+    alert("Flase, I don't play biano ");
+    }
+    if (q3 === 'no' || q3 === 'n' ){
+    alert("True, I don't play biano");
+    }
+    
+
+let q4 = prompt("Do you think I hate dogs ? Answer by Yes / No / Y / N");
+q4 = q4.toLowerCase();
+console.log(q4);
+if (q4 === 'yes' || q4 === 'y' ){
+    alert("False,  I like dogs");
+    }
+    if (q4 === 'no' || q4 === 'n' ){
+    alert("True, I like dogs");
+    }
+  
+let q5 = prompt("Do you think I like summer ? Answer by Yes / No / Y / N");
+q5 = q5.toLowerCase();
+console.log(q5);
+if (q5 === 'yes' || q5 === 'y' ){
+    alert("True, I like summer");
+    }
+    if (q5 === 'no' || q5 === 'n' ){
+    alert("False, I like summer");
+    }
+   
+    
